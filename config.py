bbfb58b4303bb5f798c23eca756c24e7c0abe448
Lerro <makar1212tg@gmail.com>
2026-01-07 15:33:20 +0400

    Change BOT_TOKEN in config.py
    Updated the BOT_TOKEN to a new value.

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = getenv("7255322374:AAFXN1LJip9tAdNz2fPbAEn94-8WPOwCumU")
+BOT_TOKEN = getenv("8525511856:AAHVCpB4pKSj9n4WR_OCFtYVh6gDWVQ2LzU")
